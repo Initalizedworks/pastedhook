5ba4c3966a817077a57d3ec3ad1ebd4bedecff6a
ARandomlyGeneratedUsername <97367244+ARandomlyGeneratedUsername@users.noreply.github.com>
2022-07-21 18:06:05 -0500

    Update Shutdown.cpp

--- src/hooks/Shutdown.cpp
+++ src/hooks/Shutdown.cpp
@@ -48,7 +48,8 @@ DEFINE_HOOKED_METHOD(Shutdown, void, INetChannel *this_, const char *reason)
     if (autoabandon && !ignoredc)
         tfmm::disconnectAndAbandon();
     ignoredc = false;
-    votelogger::Reset();
+    std::string message = reason;
+    votelogger::onShutdown(message);
     if (*random_name)
     {
         if (randomnames_file.TryLoad("names.txt"))
