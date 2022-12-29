a9387ddf4d5cb9b1f48d3c892e13dc49be4e60b1
yayo87495 <101436287+yayo87495@users.noreply.github.com>
2022-12-28 21:22:22 -0800

    Update ESP.cpp

--- src/hacks/ESP.cpp
+++ src/hacks/ESP.cpp
@@ -1446,7 +1446,6 @@ void _FASTCALL Draw3DBox(CachedEntity *ent, const rgba_t &clr)
     corners[7] = mins + Vector(0, y, z);
 
     // Rotate the box and check if any point of the box isnt on the screen
-    bool success = true;
     for (int i = 0; i < 8; i++)
     {
         float yaw    = NET_VECTOR(RAW_ENT(ent), netvar.m_angEyeAngles).y;
@@ -1459,13 +1458,8 @@ void _FASTCALL Draw3DBox(CachedEntity *ent, const rgba_t &clr)
         corners[i] += origin;
 
         if (!draw::WorldToScreen(corners[i], points[i]))
-            success = false;
+            return;
     }
-
-    // Don't continue if a point isn't on the screen
-    if (!success)
-        return;
-
     rgba_t draw_clr = clr;
     // Draw the actual box
     for (int i = 1; i <= 4; i++)
@@ -1610,17 +1604,12 @@ bool GetCollide(CachedEntity *ent)
         points_r[5] = mins + Vector(x, 0, z);
         points_r[6] = mins + Vector(x, y, z);
         points_r[7] = mins + Vector(0, y, z);
-
-        // Check if any point of the box isnt on the screen
-        bool success = true;
+        
         for (int i = 0; i < 8; i++)
         {
             if (!draw::WorldToScreen(points_r[i], points[i]))
-                success = false;
+                return false;
         }
-        // If a point isnt on the screen, return here
-        if (!success)
-            return false;
 
         // Get max and min of the box using the newly created screen vector
         int max_x = -1;
